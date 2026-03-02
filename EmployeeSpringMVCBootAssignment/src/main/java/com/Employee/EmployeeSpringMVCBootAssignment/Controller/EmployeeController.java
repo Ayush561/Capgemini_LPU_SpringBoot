076da2caf617b07510b119061446da56ea4c1184
Ayush561 <ayushans56@gmail.com>
2026-03-02 20:41:36 +0530

    Assignment

--- EmployeeSpringMVCBootAssignment/src/main/java/com/Employee/EmployeeSpringMVCBootAssignment/Controller/EmployeeController.java
+++ EmployeeSpringMVCBootAssignment/src/main/java/com/Employee/EmployeeSpringMVCBootAssignment/Controller/EmployeeController.java
@@ -5,8 +5,8 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
 import org.springframework.web.servlet.ModelAndView;
 
 import com.Employee.EmployeeSpringMVCBootAssignment.Entity.Employee;
@@ -38,12 +38,31 @@ public class EmployeeController {
 	    } else {
 
 	        jpa.save(emp);
-
-	        ModelAndView m = new ModelAndView();
-	        m.setViewName("success");
-	        return m;
+	        return new ModelAndView("success");
 	    }
 	}
+	
+	
+	//There is another way to do it using Model<Interface>, In this we don't use setview we return the jps page and we use addAttribute instead of addObject
+//	@PostMapping("/create-account")
+//	public String createAccount(@ModelAttribute Employee emp,Model model) {
+//
+//	    Optional<Employee> e = jpa.findById(emp.getEmail());
+//
+//	    if (e.isPresent()) {
+//
+//	        //ModelAndView m = new ModelAndView();
+//	        model.addAttribute("emp",e.get());
+//	        //m.setViewName("userexist");
+//	        return "userexist";
+//
+//	    } else {
+//
+//	        jpa.save(emp);
+//	        //return new ModelAndView("success");
+//	        return "success";
+//	    }
+//	}
 	
 	@GetMapping("/check-user")
 	public String checkUser() {
@@ -65,22 +84,89 @@ public class EmployeeController {
 				List<Employee> list = jpa.findAll();
 				
 				ModelAndView m = new ModelAndView();
-				m.addObject("msg", list);
+				m.addObject("employees", list);
+				m.addObject("isAdmin", true);
 				m.setViewName("listEmployee");
 				return m;
 			}
 			else {
 				ModelAndView m = new ModelAndView();
-				m.addObject("emp",validEmp);
+				m.addObject("emp", validEmp);
 				m.setViewName("loginsuccess");
 				return m;
 			}
 		}
 		else {
 			ModelAndView m = new ModelAndView();
-			m.setViewName("validate");
 			m.addObject("error", "Invalid Email or Password");
+			m.setViewName("validate");
 			return m;
 		}
 	}
+	
+	
+	@PostMapping("/delete-selected")
+	public ModelAndView deleteSelected(
+			@RequestParam(value = "selectedEmails", required = false) List<String> emails) {
+
+		if (emails != null) {
+			for (String email : emails) {
+				jpa.deleteById(email);
+			}
+		}
+
+		List<Employee> list = jpa.findAll();
+
+		ModelAndView m = new ModelAndView();
+		m.addObject("employees", list);
+		m.addObject("isAdmin", true);
+		m.setViewName("listEmployee");
+
+		return m;
+	}
+	
+	
+	@GetMapping("/update/{email}")
+	public ModelAndView openUpdatePage(@PathVariable String email) {
+
+	    Optional<Employee> emp = jpa.findById(email);
+
+	    ModelAndView m = new ModelAndView();
+
+	    if(emp.isPresent()) {
+	        m.addObject("emp", emp.get());
+	        m.setViewName("updateEmployee");
+	    }
+
+	    return m;
+	}
+	
+	
+	@PostMapping("/update-employee")
+	public ModelAndView updateEmployee(@ModelAttribute Employee emp) {
+
+	    Optional<Employee> existing = jpa.findById(emp.getEmail());
+
+	    if(existing.isPresent()) {
+
+	        Employee e = existing.get();
+
+	        e.setName(emp.getName());
+	        e.setRole(emp.getRole());
+	        e.setSalary(emp.getSalary());
+
+	        jpa.save(e);
+	    }
+
+	    List<Employee> list = jpa.findAll();
+
+	    ModelAndView m = new ModelAndView();
+	    m.addObject("employees", list);
+	    m.addObject("isAdmin", true);
+	    m.setViewName("listEmployee");
+
+	    return m;
+	}
+	
+	
 }
